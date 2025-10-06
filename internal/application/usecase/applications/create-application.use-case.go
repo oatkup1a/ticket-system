1df94249c6b5e8edf3363cb30a58dd0458d7d34a
oatkup1a <voraphol12345@gmail.com>
2025-10-06 10:55:21 +0900

    error code

--- internal/application/usecase/applications/create-application.use-case.go
+++ internal/application/usecase/applications/create-application.use-case.go
@@ -2,6 +2,7 @@ package applications
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/google/uuid"
 	"oatkup.sys/internal/entity"
@@ -15,13 +16,15 @@ func NewCreateApplicationUsecase(r applications.Repository) *CreateApplicationUs
 }
 
 func (uc *CreateApplicationUsecase) Apply(ctx context.Context, req *ApplicationRequest) (*ApplicationModel, error) {
+	const errPrefix = "[CreateApplicationUsecase][Apply]"
+
 	app := &entity.Application{
 		ID:          uuid.NewString(),
 		Name:        req.Name,
 		Description: req.Description,
 	}
 	if err := uc.repo.Create(ctx, app); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("%s: %w", errPrefix, err)
 	}
 	return &ApplicationModel{
 		ID:          app.ID,
